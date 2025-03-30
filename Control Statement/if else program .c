d567b2b0a8c69bf59a86a7844e055889a3d5cfe7
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-30 08:26:48 +0530

    Control statement

---
+++ Control Statement/if else program .c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num=10;
+
+    if(Num<100)
+
+    {
+        printf("\n GOOD ");
+    }
+
+    else
+
+    {
+        printf("\n BYE ");
+
+    }
+
+    getch();
+
+    return 0;
+
+}
