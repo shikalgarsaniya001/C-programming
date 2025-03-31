92ade9aeb91d2faabdb6ea632808d3f71fcedf02
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-03-31 07:25:48 +0530

    Control statement

---
+++ Control Statement/0.2 if else program .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int No=10;
+
+    if(No>100)
+
+
+     printf("\n YC CLG");
+
+
+     else
+     {
+         printf("\n FORK");
+     }
+     getch();
+     return 0;
+
+
+}
