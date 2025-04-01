4afe45d2a9531a7d84fe60055ee43f2df3609107
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-01 07:51:18 +0530

    Control statement

---
+++ Control Statement/0.3 if else program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num=0;
+
+    if(Num<100)
+    {
+        printf("\n SANIYA");
+    }
+    else
+    {
+        printf("\n Good");
+    }
+    getch();
+    return 0;
+
+}
