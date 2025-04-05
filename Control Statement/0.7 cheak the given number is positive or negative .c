2c945ca0199fa9abb8d0d4333c031cb6f6871f4b
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-05 07:22:47 +0530

    Control statement

---
+++ Control Statement/0.7 cheak the given number is positive or negative .c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num=0;
+
+    printf("\n Enter a number :");
+    scanf("\n %d",&Num);
+
+    if(Num==0)
+    {
+        printf("\n The given number is neutral");
+    }
+
+     else if(Num>0)
+    {
+        printf("\n The given number is positive");
+    }
+
+    else
+    {
+        printf("\n The given number is negative");
+    }
+
+    getch();
+    return 0;
+}
