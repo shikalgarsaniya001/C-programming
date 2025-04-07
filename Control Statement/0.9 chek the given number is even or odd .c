8a4b2fede87a55129d1d676b5a53b4712ee75bee
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-07 10:08:25 +0530

    Control statement

---
+++ Control Statement/0.9 chek the given number is even or odd .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num=10;
+
+    printf("\n **************************");
+
+    if(Num%2==0)
+
+    {
+        printf("\n Number %d is Even",Num);
+    }
+
+    else
+
+    {
+        printf("\n  Number %d is Odd",Num);
+    }
+
+    printf("\n **************************");
+
+    getch();
+    return 0;
+}
