546f6f0521d7ae67446cf179fc4fe2f3cddd7c62
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-20 07:02:51 +0530

    Ternary program

---
+++ Ternary Operator/0.20 Ternary simple program.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num =0;
+    printf("\n ******************************");
+
+    printf("\n Enter a Num :");
+    scanf("\n %d",&Num);
+
+    (Num<100)? printf("\n SANIYA") :printf("\n FORK");
+
+    printf("\n ******************************");
+
+    getch();
+
+    return 0;
+}
+
+
