8585b51fd4b885d11615cedad2c8b1d7b1e9f9e7
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-26 06:34:15 +0530

    Control Stement

---
+++ Control Statement/0.27 Addition of two number .c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int a,b, Sum;
+
+    printf("\n ****************************");
+
+    printf("\n Enter a 1st number :");
+    scanf("%d",&a);
+
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&b);
+
+    printf("\n ****************************");
+
+    Sum = a + b;
+
+    printf("\n Addition of %d + %d= %d ",a,b,Sum);
+
+    printf("\n ****************************");
+
+    getch();
+
+    return 0;
+}
