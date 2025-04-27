09b1aa9bcfea6bcb80969c050a71ba357024b43a
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-27 07:27:14 +0530

    Control Statement

---
+++ Control Statement/0.28 sub of givrn to number.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int a,b, Sub;
+
+    printf("\n ****************************");
+
+    printf("\n Enter a 1st number :");
+    scanf("%d",&a);
+
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&b);
+
+    printf("\n ****************************");
+
+    Sub = a - b;
+
+    printf("\n Substraction  of %d - %d= %d ",a,b,Sub);
+
+    printf("\n ****************************");
+
+    getch();
+
+    return 0;
+}
+
