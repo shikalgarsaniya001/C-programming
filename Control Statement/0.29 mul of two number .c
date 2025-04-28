af824e0128d57568715fb0cdb5d6ad90eb660cf4
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-28 07:52:42 +0530

    Control Statement

---
+++ Control Statement/0.29 mul of two number .c
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int a,b, Mul;
+
+    printf("\n ****************************");
+
+    printf("\n Enter a 1st number :");
+    scanf("%d",&a);
+
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&b);
+
+    printf("\n ****************************");
+
+    Mul = a * b;
+
+    printf("\n Multiplication of %d * %d= %d ",a,b,Mul);
+
+    printf("\n ****************************");
+
+    getch();
+
+    return 0;
+}
