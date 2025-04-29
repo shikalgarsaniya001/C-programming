6760b5caabeb9c9e5ad859942a150414fe1ae592
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-29 06:23:24 +0530

    Control Statement

---
+++ Control Statement/0.30 div of two number .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+{
+    int a,b, Div;
+
+    printf("\n ****************************");
+
+    printf("\n Enter a 1st number :");
+    scanf("%d",&a);
+
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&b);
+
+    printf("\n ****************************");
+
+    Div = a / b;
+
+    printf("\n DIV of %d * %d= %d ",a,b,Div);
+
+    printf("\n ****************************");
+
+    getch();
+
+    return 0;
+}
+
