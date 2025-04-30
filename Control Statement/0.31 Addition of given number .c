3f27ef054a75f014b33166f3017b41bb1a96346e
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-04-30 06:29:15 +0530

    Control statement

---
+++ Control Statement/0.31 Addition of given number .c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdio.h>
+int main()
+
+{
+    int a,b,c,d,e,f,Sum;
+
+    printf("\n **********************************************************************");
+
+    printf("\n Enter a 1st number :");
+    scanf("%d",&a);
+
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&b);
+
+    printf("\n Enter a 3rd number :");
+    scanf("%d",&c);
+
+    printf("\n Enter a 4th number :");
+    scanf("%d",&d);
+
+    printf("\n Enter a 5th number :");
+    scanf("%d",&e);
+
+    printf("\n Enter a 6th number :");
+    scanf("%d",&f);
+
+
+    printf("\n **********************************************************************");
+
+    Sum = a + b + c + d + e + f ;
+
+    printf("\n Addition of %d + %d + %d + %d + %d + %d= %d ",a,b,c,d,e,f,Sum);
+
+    printf("\n **********************************************************************");
+
+    getch();
+
+    return 0;
+}
+
+
+
+
