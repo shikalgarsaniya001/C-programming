bf1f50f7a67c42d9ff8af267ccf47203ad052df4
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-05-04 07:22:25 +0530

    Control Statement

---
+++ Control Statement/0.35 Squ of the given number .c
@@ -0,0 +1,28 @@
+/*#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n -----------&&&&&&&&&&&&&---------------");
+    int No,sqr;
+    printf("\n Enter any number :");
+    scanf("%d",&No);
+
+    sqr=No*No;
+    printf("\n square of %d is = %d",No,sqr);
+     printf("\n -----------&&&&&&&&&&&&&---------------");
+     getch();
+     return 0;
+
+}*/
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No=4, Sqr=0;
+
+    Sqr = 4 * 4;
+}
+
+
