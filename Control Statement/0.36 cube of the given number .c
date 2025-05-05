373b47060828530a714a0bc44434bcbccf84f28c
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-05-05 08:11:31 +0530

    Control Statement

---
+++ Control Statement/0.36 cube of the given number .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\n -----------&&&&&&&&&&&&&---------------");
+
+    int No,Cube;
+
+    printf("\n Enter any number :");
+
+    scanf("%d",&No);
+
+    Cube = No*No*No;
+
+    printf("\n Cube of %d  is = %d ",No,Cube);
+
+    printf("\n -----------&&&&&&&&&&&&&---------------");
+
+    getch();
+
+    return 0;
+
+}
+
+
+
+
