3118fafd956a30cf629db2c688f480c740681730
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-05-06 04:17:11 +0530

    Control Statement

---
+++ Control Statement/0.37 leap year prograam .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num=2000 , Lyear  =0;
+
+
+    Lyear  = 2000 % 4;
+
+    printf("\n The 2000  number is leap year");
+
+    return 0;
+
+
+}
+
