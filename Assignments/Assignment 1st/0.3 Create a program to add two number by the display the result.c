ad9b3b3f2ba4a019ac6eb1821d7475672f4b7924
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-05-25 10:25:04 +0530

    Addition

---
+++ Assignments/Assignment 1st/0.3 Create a program to add two number by the display the result.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1= 8, No2 = 15 ,sum=0;
+
+
+    sum = No1+ No2;
+
+    printf("\n Addition of the given number  is  %d",sum);
+    getch();
+
+    return 0;
+
+}
+
+
