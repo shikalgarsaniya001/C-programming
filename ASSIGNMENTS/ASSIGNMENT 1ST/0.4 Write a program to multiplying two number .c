72ab4aff81533f30bb9584b4c8981209f9745e3a
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-05-28 11:23:28 +0530

    Mul

---
+++ ASSIGNMENTS/ASSIGNMENT 1ST/0.4 Write a program to multiplying two number .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int No1= 6, No2 = 7 ,Mul=0;
+
+
+        Mul= No1* No2;
+
+    printf("\n Multiplication of the given number  is  %d",Mul);
+    getch();
+
+    return 0;
+
+}
+
+
