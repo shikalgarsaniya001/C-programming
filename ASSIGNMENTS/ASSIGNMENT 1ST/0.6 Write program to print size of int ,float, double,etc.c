04b346a1854b7892e2bf77fbfb67cca49fe172d4
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-05-30 07:10:14 +0530

    size

---
+++ ASSIGNMENTS/ASSIGNMENT 1ST/0.6 Write program to print size of int ,float, double,etc.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+
+
+
+printf("\n size of char is %d",sizeof(char));
+printf("\n size of int is %d",sizeof(int));
+printf("\n size of float is %d",sizeof(float));
+printf("\n size of double is %d",sizeof(double));
+
+
+    return 0;
+
+}
+
+
+
