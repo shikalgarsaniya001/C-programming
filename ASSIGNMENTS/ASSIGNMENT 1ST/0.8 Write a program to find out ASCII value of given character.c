cc4ca1c07239278fb5a2be34db781f7a4a44b318
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-05-31 07:09:33 +0530

    ASCII value

---
+++ ASSIGNMENTS/ASSIGNMENT 1ST/0.8 Write a program to find out ASCII value of given character.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch='/0';
+
+    printf("\n Enter a character :");
+    scanf("%c",ch);
+
+    printf("\n ASCII table value for alphabets %c is %d",ch,ch);
+
+    printf("\n Thank you");
+
+    return 0;
+}
