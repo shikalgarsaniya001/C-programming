1b2495076126be19fef676a8d98d636b59d13df2
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-06-01 06:57:23 +0530

    ASCII TABLE

---
+++ ASSIGNMENTS/ASSIGNMENT 1ST/0.9 Write a program print  ASCII table .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+    char ch='/0';
+
+    printf("\n Enter a value :");
+    scanf("%c",num);
+
+    printf("\n ASCII table value for alphabets %c is %d",ch,num);
+
+    printf("\n Thank you");
+
+    return 0;
+}
+
