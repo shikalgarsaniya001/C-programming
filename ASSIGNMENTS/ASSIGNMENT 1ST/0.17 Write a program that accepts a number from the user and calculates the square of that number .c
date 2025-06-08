318b6fc8beaa407ba691084f46d07f183f8d8295
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-06-08 08:41:09 +0530

    Square

---
+++ ASSIGNMENTS/ASSIGNMENT 1ST/0.17 Write a program that accepts a number from the user and calculates the square of that number .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int  num, sqr;
+
+    printf("\n ____________________________________________");
+    printf("\n Enter a number :");
+    scanf("\n %d",&num);
+
+    sqr= num*num;
+
+    printf("\n Sqr of the given number is %d", sqr);
+
+    printf("\n ____________________________________________");
+
+     getch();
+     return 0;
+}
