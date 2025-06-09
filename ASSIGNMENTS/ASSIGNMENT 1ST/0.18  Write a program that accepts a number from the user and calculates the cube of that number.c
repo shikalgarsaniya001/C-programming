f8be1f2a63a65f6e989c9840898ba7d5cdb5ee0f
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-06-09 08:12:06 +0530

    cube

---
+++ ASSIGNMENTS/ASSIGNMENT 1ST/0.18  Write a program that accepts a number from the user and calculates the cube of that number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int  num, cube;
+
+    printf("\n ____________________________________________");
+
+    printf("\n Enter a number :");
+    scanf("\n %d",&num);
+
+    cube= num*num*num;
+
+    printf("\n cube of the given number is %d", cube);
+
+    printf("\n ____________________________________________");
+
+     getch();
+     return 0;
+}
+
