7480cec3da9928acc99a5b6c28523ef65cfbf1d7
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-07-02 11:39:58 +0530

    if else

---
+++ ASSIGNMENTS/ASSIGNMENTS 2nd/0.17 write a program toaccept a character from user and convert case (toggle case)of that character .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   printf("\n I LOVE YOU OMKAR\n");
+   printf("\n MISS YOU \n");
+
+   printf("\n YOU are the MOST important in my LIFE");
+   getch();
+}
