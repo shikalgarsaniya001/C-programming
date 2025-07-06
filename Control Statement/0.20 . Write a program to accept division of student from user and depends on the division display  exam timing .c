1e954243b7ceedcefede659677470df3457b3d75
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-07-06 09:42:36 +0530

    if else

---
+++ Control Statement/0.20 . Write a program to accept division of student from user and depends on the division display  exam timing .c
@@ -0,0 +1,114 @@
+/*#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+
+    printf("\n There are 4 division in school as A,B,C,D\n");
+
+    printf("\n Enter a division :");
+    scanf("\n %c",&ch);
+
+    switch(ch)
+    {
+        case 'A' :
+             {
+               printf("\n your exam is at 10 AM");
+               break;
+            }
+        case 'a':
+            {
+               printf("\n your exam is at 10 AM");
+               break;
+            }
+
+        case'B':
+            {
+                printf("\n your exam is at 10.30 AM");
+                break;
+            }
+        case'b':
+            {
+                printf("\n your exam is at 10.30 AM");
+                break;
+            }
+        case'C':
+            {
+                printf("\n your exam is at 1 PM");
+                break;
+            }
+        case'c':
+            {
+                printf("\n your exam is at 1 PM");
+                break;
+            }
+        case'D':
+            {
+                printf("\n your exam is at 1.30 PM");
+                break;
+            }
+        case'd':
+            {
+                printf("\n your exam is at 1.30 PM");
+                break;
+            }
+
+
+    }
+            getch();
+            return 0;
+
+}*/
+
+///OR///
+
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch;
+
+    printf("\n There are 4 division in school as A,B,C,D\n");
+
+    printf("\n Enter a division :");
+    scanf("\n %c",&ch);
+
+    switch(ch)
+    {
+        case 'A' :
+        case 'a':
+             {
+               printf("\n your exam is at 10 AM");
+               break;
+             }
+
+
+        case'B':
+        case'b':
+
+            {
+                printf("\n your exam is at 10.30 AM");
+                break;
+            }
+
+        case'C':
+        case'c':
+
+            {
+                printf("\n your exam is at 1 PM");
+                break;
+            }
+        case'D':
+        case'd':
+            {
+                printf("\n your exam is at 1.30 PM");
+                break;
+            }
+
+    }
+            getch();
+            return 0;
+
+}
+
