41ffe9d3c9e5bef5967b924cda85e074c5a3a634
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-07-13 08:00:57 +0530

    loop

---
+++ ASSIGNMENTS/ASSIGNMENT 3rd/0.3 write a program to accept number and print  .c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int ni,num;
+
+   printf("\n Enter a number :");
+   scanf("\n %d",&num);
+
+   for(int i=1;i<=num;i++)
+   {
+       printf(" * ");
+   }
+   getch();
+   return 0;
+}
