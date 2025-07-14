6702eef078983af3ccc418f16104a942745758cb
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-07-14 08:09:51 +0530

    loop

---
+++ ASSIGNMENTS/ASSIGNMENT 3rd/0.5 Write a program which accept two number from user and dislay first number in secound number of times .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num1,num2,i;
+
+    printf("\n Enter a number :");
+    scanf("\n %d",&num1);
+
+    printf("\n Enter a number ");
+    scanf("\n %d",&num2);
+
+    for(i=1;i<=num1;i++)
+    {
+        printf("\n %d",num2);
+    }
+    getch();
+    return 0;
+}
