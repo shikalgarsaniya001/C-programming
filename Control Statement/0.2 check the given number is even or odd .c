8e15d5fa166eb7bf14d264c6b75b9efb3316bdc9
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-07-27 09:47:30 +0530

    if else

---
+++ Control Statement/0.2 check the given number is even or odd .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+
+    printf("\n Enter a number :");
+    scanf("\n %d",&num);
+
+    if(num%2==0)
+    {
+        printf("\n The given number %d is Even",num);
+    }
+    else{
+        printf("\n The given number %d is odd",num);
+    }
+
+    getch();
+    return 0;
+}
