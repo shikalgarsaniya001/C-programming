e00f6110f452591e3a05d75b62cb37d25a8b5fcb
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-08-03 10:29:20 +0530

    control statement

---
+++ Control Statement/0.11 write a program to lees than 10 print hello and otherwise world .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int num;
+
+    printf("\n Enter a number :");
+    scanf("\n %d",&num);
+
+    if(num<=10)
+    {
+        printf("\n HELLO");
+    }
+    else
+    {
+        printf("\n WORLD");
+    }
+    getch();
+    return 0;
+}
