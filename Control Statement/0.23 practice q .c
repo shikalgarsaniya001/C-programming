7d036f6bcf26cecbac0dd1e5216a99000b4f5eee
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-08-10 07:13:24 +0530

    control statement

---
+++ Control Statement/0.23 practice q .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x=2;
+
+    if(x=4)
+    {
+        printf("\n X is equal to 1");
+        printf("\n\n X = %d",x);
+    }
+    else
+    {
+        printf("\n X is not equal to 1");
+    }
+    return 0;
+}
