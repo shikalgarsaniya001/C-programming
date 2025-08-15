474d662324ef82bc58104c3b0dfaed9faf685da1
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-08-15 15:01:17 +0530

    loop

---
+++ Loop/0.2 print the hello using a for loop .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int i ,x;
+
+   printf("\n Enter a number :");
+   scanf("\n %d",&x);
+
+   for(i=1; i<=x;i++)
+   {
+       printf("\n HELLO");
+   }
+   getch();
+   return 0;
+}
