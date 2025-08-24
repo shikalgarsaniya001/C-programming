13df5b7133bb88df101765eee67e0c9f297221a1
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-08-24 09:58:15 +0530

    loop

---
+++ Loop/0.9 Accept the number and print hello using while loop .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=1,n;
+
+    printf("\n Enter a number :");
+    scanf("\n %d",&n);
+
+    while(i<=n)
+    {
+        printf("\n SANIYA");
+        i++;
+    }
+    getch();
+    return 0;
+}
