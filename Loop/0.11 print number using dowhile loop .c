cc0ec3af117974f002a11ff11519bf2945bd3aae
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-08-27 08:37:38 +0530

    loop

---
+++ Loop/0.11 print number using dowhile loop .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int i=1;
+
+    do{
+        printf("\n %d",i);
+        i++;
+    }
+    while(i<=5);
+
+        getch();
+       return 0;
+}
