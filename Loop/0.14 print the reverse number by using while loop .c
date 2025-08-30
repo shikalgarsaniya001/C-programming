bf82eeb7f40eabf34f2a344900ee80072696c475
shikalgarsaniya001 <shikalgarsaniya001@gmail.com>
2025-08-30 10:43:28 +0530

    loop

---
+++ Loop/0.14 print the reverse number by using while loop .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  /*  int i =10;
+
+    while(i>=1)
+    {
+        printf("\n %d",i);
+        i--;
+    }
+    getch();
+    return 0;*/
+
+    int x,i=1;
+
+    printf("\n Enter a number :");
+    scanf("\n %d",&x);
+
+    while(x<=i)
+    {
+        printf("\n %d",i);
+        i--;
+    }
+    getch();
+    return 0;
+}
